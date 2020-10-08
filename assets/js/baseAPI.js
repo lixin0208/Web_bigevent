82524ace4ab71c7d2145f1db25d8846fe6716386
itheima <itheima@itcast.cn>
2020-10-08 20:17:12 +0800

    注册和登录功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,9 +1,9 @@
 //1.开发环境服务器地址
-var baseURL = "http://ajax.frontend.itheima"
+var baseURL = "http://ajax.frontend.itheima.net"
 //2.测试环境服务器地址
-var baseURL = "http://ajax.frontend.itheima"
+var baseURL = "http://ajax.frontend.itheima.net"
 //3.生产环境服务器的地址
-var baseURL = "http://ajax.frontend.itheima"
+var baseURL = "http://ajax.frontend.itheima.net"
 //拦截所有ajax请求:get/post/ajax;
 //处理参数
 $.ajaxPrefilter(function (params) {
